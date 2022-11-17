f6b911813f57ef3f111cbeb769e436eaae95125c
pravinnarote <114488712+pravinnarote@users.noreply.github.com>
2022-11-17 09:42:30 +0530

    Create Bubble_sort.py

---
+++ Bubble_sort.py
@@ -0,0 +1,21 @@
+20 lines (14 sloc)  422 Bytes
+#bubble sort
+def bubble(array):
+    for i in range(len(array)):
+        for j in range(0,len(array)-i-1):
+            if(array[j]>array[j+1]):
+                temp=array[j]
+                array[j]=array[j+1]
+                array[j+1]=temp
+
+
+
+
+
+
+array=[]
+n=int(input("ENTER THE NUMBER OF ELEMENTS IN THE ARRAY : "))
+for i in range(n):
+    array.append(int(input("PLEASE ENTER THE VALUES : ")))
+bubble(array)
+print(array)
